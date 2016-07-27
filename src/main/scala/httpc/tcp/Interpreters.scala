ea8dfc1c266890a4cc56582d89785040f1c3ef2b
Amr Hassan <amr.hassan@gmail.com>
2016-07-27 11:01:10 +0200

    tcp: immutable connection cache

--- src/main/scala/httpc/tcp/Interpreters.scala
+++ src/main/scala/httpc/tcp/Interpreters.scala
@@ -1,6 +1,5 @@
 package httpc.tcp
 
-import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
 import cats.data.XorT
 import httpc.tcp.sockets.{Addresses, Socket, SocketError}
@@ -12,10 +11,9 @@ import httpc.tcp.TcpError.ConnectionNotFound
 object Interpreters {
 
   /** Interpreter for TCP language using OS sockets */
-  def socketsInterpreter(implicit ec: ExecutionContext): TcpNet.Interpreter =
-    new TcpNet.Interpreter {
+  def socketsInterpreter(implicit ec: ExecutionContext): TcpNet.Interpreter = new TcpNet.Interpreter {
 
-    val socketMap = mutable.Map.empty[ConnectionId, Socket]
+    var cons = Map.empty[ConnectionId, Socket]
 
     def apply[A](fa: TcpNetOp[A]): XorT[Future, TcpError, A] = XorT.fromXor[Future](fa match {
       case AddrLookup(hostname) ⇒
@@ -23,26 +21,26 @@ object Interpreters {
       case Connect(address, port) ⇒
         for {
           socket <- Socket.connect(address, port) leftMap errorTranslate
-          id = socketId(socket)
-        } yield { socketMap(id) = socket; id }
+          id = conId(socket)
+        } yield { cons = cons.updated(id, socket); id }
       case Disconnect(id) ⇒
         for {
-          socket ← socketMap.get(id).toRightXor(ConnectionNotFound(id))
+          socket ← cons.get(id).toRightXor(ConnectionNotFound(id))
           _ ← socket.disconnect() leftMap errorTranslate
-        } yield { socketMap.remove(id); () }
+        } yield { cons = cons - id; () }
       case Read(id, length) ⇒
         for {
-          socket ← socketMap.get(id).toRightXor(ConnectionNotFound(id))
+          socket ← cons.get(id).toRightXor(ConnectionNotFound(id))
           data ← socket.read(length) leftMap errorTranslate
         } yield data
       case Write(id, data) ⇒
         for {
-          socket ← socketMap.get(id).toRightXor(ConnectionNotFound(id))
+          socket ← cons.get(id).toRightXor(ConnectionNotFound(id))
           _ ← socket.write(data) leftMap errorTranslate
         } yield ()
     })
 
-    def socketId(socket: Socket): ConnectionId =
+    def conId(socket: Socket): ConnectionId =
       ConnectionId(socket.socket.getPort | (socket.socket.getLocalPort << 16))
 
     def errorTranslate(socketError: SocketError): TcpError = socketError match {
