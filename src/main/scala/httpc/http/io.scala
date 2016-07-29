4cb512e71edb11f0ece79e7e4caa40aa2a4c0258
Amr Hassan <amr.hassan@gmail.com>
2016-07-29 20:14:15 +0200

    Fixed broken scaladocs

--- src/main/scala/httpc/http/io.scala
+++ src/main/scala/httpc/http/io.scala
@@ -12,21 +12,17 @@ object HttpIo {
   def run[A](command: HttpIo[A], netInt: NetIo.Interpreter): XorT[Future, HttpError, A] =
     command.run(netInt)
 
-  /** Lifts a [[NetIo]] into an [[HttpIo]] */
   def fromNetIo[A](command: NetIo[A])(implicit ec: ExecutionContext): HttpIo[A] = Kleisli {
     (netInt: NetIo.Interpreter) ⇒
       NetIo.run(command, netInt) leftMap (e ⇒ HttpError.NetworkError(e.show))
   }
 
-  /** Lifts a pure value into [[HttpIo]] */
   def pure[A](a: A): HttpIo[A] =
     Kleisli(_ ⇒ XorT(Future.successful(Xor.right(a))))
 
-  /** Lifts an error into [[HttpIo]] */
   def error[A](e: HttpError): HttpIo[A] =
     Kleisli(_ ⇒ XorT(Future.successful(Xor.left(e))))
 
-  /** Lifts an [[Xor]] into an [[HttpIo]] */
   def xor[A](fa: HttpError Xor A): HttpIo[A] =
     Kleisli(_ ⇒ XorT(Future.successful(fa)))
 }
